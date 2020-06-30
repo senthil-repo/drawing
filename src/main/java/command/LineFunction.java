e2438e6d3e8f8a588c921b2f365e9f56fd597a39
senthilnathan.natesan@gmail.com <Mauriya123>
2020-06-30 17:19:15 +0100

    Added LineFunction implementation.

--- src/main/java/command/LineFunction.java
+++ src/main/java/command/LineFunction.java
@@ -1,6 +1,8 @@
 package command;
 
 import domain.Canvas;
+import domain.Constants;
+import domain.Line;
 
 /**
  * Created on 30/06/2020.
@@ -8,6 +10,15 @@ import domain.Canvas;
 public class LineFunction implements CanvasFunction {
     @Override
     public void applyCanvasFunction(Canvas canvas, String[] params) {
+        Line line = new Line(params);
+        fillWithLine(line, canvas.getCanvasArray());
+    }
 
+    private void fillWithLine(Line line, char[][] canvasArray) {
+        for(int i = line.getX1() - 1; i < line.getX2() ; i++ ) { //row
+            for(int j = line.getY1() - 1; j < line.getY2(); j++) { //column
+                canvasArray[i][j] = Constants.LINE;
+            }
+        }
     }
 }
--- src/main/java/domain/Constants.java
+++ src/main/java/domain/Constants.java
@@ -5,4 +5,8 @@ package domain;
  */
 public class Constants {
     public static final String ERROR_MESSAGE_CREATE_CANVAS = " Invalid parameters - unable to create canvas. Two positive integers (width & height) are required. ";
+
+    public static final char LINE = 'x';
+    public static final char HORIZONTAL = '-';
+    public static final char VERTICAL = '|';
 }
