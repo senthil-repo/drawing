9f947d37a02c8ad8da2f77b22b6674efd1a5ad69
senthilnathan.natesan@gmail.com <Mauriya123>
2020-07-01 03:50:34 +0100

    Adding more edge test cases

--- src/test/java/render/DrawTest.java
+++ src/test/java/render/DrawTest.java
@@ -1,6 +1,7 @@
 package render;
 
 import domain.Canvas;
+import domain.Constants;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -41,4 +42,81 @@ public class DrawTest {
         return builder.toString().trim();
     }
 
+    @Test
+    public void testDraw_Line() {
+        Canvas canvas = getInputCanvas();
+        addLineInsideCanvas(canvas);
+
+        String result = draw.draw(canvas);
+        assertEquals(" Unexpected drawing ",getExpectedCanvas_With_Line(), result);
+    }
+
+    @Test
+    public void testDraw_Rectangle() {
+        Canvas canvas = getInputCanvas();
+        addRectangleInsideCanvas(canvas);
+
+        String result = draw.draw(canvas);
+        assertEquals(" Unexpected drawing ",getExpectedCanvas_With_Rectangle(), result);
+    }
+    private void addLineInsideCanvas(Canvas canvas) {
+        char canvasArray[][] = canvas.getCanvasArray();
+        for(int i = 0; i < 6 ; i++ ) {
+            for(int j = 1; j < 2; j++) {
+                canvasArray[i][j] = Constants.LINE;
+            }
+        }
+    }
+
+    private void addRectangleInsideCanvas(Canvas canvas) {
+        char canvasArray[][] = canvas.getCanvasArray();
+        for(int i = 5; i < 10; i++ ) {
+            canvasArray[i][0] = Constants.LINE;
+            for(int j = 0; j < 3; j++) {
+                if((i == 5 || i == 9 || j == 2)) {
+                    canvasArray[i][j] = Constants.LINE;
+                }
+            }
+        }
+    }
+
+    //TODO
+/*    private void bucketFill_InsideCanvas(Canvas canvas) {
+        char canvasArray[][] = canvas.getCanvasArray();
+        for(int i = 5; i < 10; i++ ) {
+            canvasArray[i][0] = Constants.LINE;
+            for(int j = 0; j < 3; j++) {
+                if((i == 5 || i == 9 || j == 2)) {
+                    canvasArray[i][j] = Constants.LINE;
+                }
+            }
+        }
+    }*/
+
+
+    private String getExpectedCanvas_With_Line() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("------------\n").append("|          |\n").append("|xxxxxx    |\n")
+                .append("|          |\n").append("|          |\n").append("------------\n");
+
+        return builder.toString().trim();
+    }
+
+    private String getExpectedCanvas_With_Rectangle() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("------------\n").append("|     xxxxx|\n")
+                .append("|     x   x|\n").append("|     xxxxx|\n")
+                .append("|          |\n").append("------------\n");
+
+        return builder.toString().trim();
+    }
+
+/*    private String getExpectedCanvas_With_BucketFill() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("------------\n").append("| xxxxx|\n")
+                .append("| x x|\n").append("| xxxxx|\n")
+                .append("| |\n").append("------------\n");
+
+        return builder.toString().trim();
+    }*/
 }
